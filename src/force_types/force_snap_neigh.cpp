2e0d45b4813b7fef0389212a7bf6dea505c97848
Christian Trott <crtrott@sandia.gov>
2020-10-23 16:11:09 -0700

    Atempt to make ExaMiniMD work with OpenMPTarget
    This compiles now (leaving out the SNAP potential which requires,
    ThreadVector level paralle_scan), but fails to do mandatory offload
    for a couple kernels (sorting, neighborlist construction) in the LJ example.

--- src/force_types/force_snap_neigh.cpp
+++ src/force_types/force_snap_neigh.cpp
@@ -36,8 +36,11 @@
 //  Questions? Contact Christian R. Trott (crtrott@sandia.gov)
 //************************************************************************
 
+#include<Kokkos_Macros.hpp>
+#ifndef KOKKOS_ENABLE_OPENMPTARGET
 #include<force_snap_neigh_impl.h>
 #define FORCETYPE_DECLARE_TEMPLATE_MACRO(NeighType) ForceSNAP<NeighType>
 #define FORCE_MODULES_TEMPLATE
 #include<modules_neighbor.h>
 #undef FORCE_MODULES_TEMPLATE
+#endif
